00cf937862d962b9ce96a038a37b510e439c78c6
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-09 19:44:40 -0500

    Add test for Calculator#subtract

--- src/test/java/co/com/sofka/app/calculator/CalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/CalculatorTest.java
@@ -15,15 +15,30 @@ class CalculatorTest {
     }
 
     @CsvSource({
-            "0,    1,   1",
-            "1,    2,   3",
-            "49,  51, 100",
-            "1,  100, 101"
+            "0, 1, 1",
+            "1, 2, 3",
+            "49, 51, 100",
+            "1, 100, 101"
     })
     @ParameterizedTest(name = "{0} plus {1} should return {2}.")
     void sum(Long a, Long b, Long expectedResult) {
         // Act
         Long actualResult = calculator.sum(a, b);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @CsvSource({
+            "45, 34, 11",
+            "1, 2, -1",
+            "100, 51, 49",
+            "23, 12, 11"
+    })
+    @ParameterizedTest(name = "{0} minus {1} should return {2}.")
+    void subtract(Long minuend, Long subtrahend, Long expectedResult) {
+        // Act
+        Long actualResult = calculator.subtract(minuend, subtrahend);
 
         // Assert
         assertEquals(expectedResult, actualResult);

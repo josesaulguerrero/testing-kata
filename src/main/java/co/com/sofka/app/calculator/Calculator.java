b9a4760847847dc783f5f9a4080a1cf8f142c2bb
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-09 19:46:03 -0500

    Implemented Calculator#subtract

--- src/main/java/co/com/sofka/app/calculator/Calculator.java
+++ src/main/java/co/com/sofka/app/calculator/Calculator.java
@@ -5,8 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Calculator {
     public Long sum(Long a, Long b) {
-        log.info("Summing {} + {}", a, b);
+        log.info("Summing {} and {}", a, b);
         return a + b;
+    }
+
+    public Long subtract(Long minuend, Long subtrahend) {
+        log.info("Subtracting {} from {}", minuend, subtrahend);
+        return minuend - subtrahend;
     }
 
     //TODO complete all arithmetical operations.
